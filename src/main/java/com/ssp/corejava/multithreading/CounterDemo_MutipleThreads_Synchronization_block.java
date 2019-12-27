1480f1243ef1fbba06378e6b222e17c6fdab46c1
sivaprasads <somarouthu.sivaprasads@gmail.com>
2019-12-27 18:34:00 +0530

    Thread Synchronization

---
+++ src/main/java/com/ssp/corejava/multithreading/CounterDemo_MutipleThreads_Synchronization_block.java
@@ -0,0 +1,67 @@
+package com.ssp.corejava.multithreading;
+
+public class CounterDemo_MutipleThreads_Synchronization_block {
+	public static void main(String[] args) throws InterruptedException {
+		Counter counter = new Counter();
+
+		Thread increment = new IncrementThread(counter);
+		Thread decrement = new DecrementThread(counter);
+
+		increment.start();
+		decrement.start();
+
+		increment.join();
+		decrement.join();
+
+		System.out.println(counter.value());
+
+	}
+
+	private static class IncrementThread extends Thread {
+		private Counter counter;
+
+		public IncrementThread(Counter counter) {
+			this.counter = counter;
+		}
+
+		public void run() {
+			for (int i = 0; i < 1000; i++) {
+				synchronized (counter) {
+					counter.increment();
+				}
+			}
+		}
+	}
+
+	private static class DecrementThread extends Thread {
+		private Counter counter;
+
+		public DecrementThread(Counter counter) {
+			this.counter = counter;
+		}
+
+		public void run() {
+			for (int i = 0; i < 1000; i++) {
+				synchronized (counter) {
+					counter.decrement();
+				}
+			}
+		}
+	}
+
+	private static class Counter {
+		private int count = 0;
+
+		public synchronized void increment() {
+			count++;
+		}
+
+		public synchronized void decrement() {
+			count--;
+		}
+
+		public int value() {
+			return count;
+		}
+	}
+}
